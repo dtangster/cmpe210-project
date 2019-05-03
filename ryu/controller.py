70ae8200a60463a95b362eab2ee48d532a0d4ab1
David Tang <dtang@egnyte.com>
2019-05-02 21:31:50 -0700

    Add throttling logic in controller

--- ryu/controller.py
+++ ryu/controller.py
@@ -1,3 +1,6 @@
+import time
+
+from ryu.app.simple_switch_13 import SimpleSwitch13
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
@@ -8,30 +11,39 @@ from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 
 
-class SimpleSwitch13(app_manager.RyuApp):
+class QoS(SimpleSwitch13):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
+        self.throttle_info = {}
+        self.meters = set()
+        self.meter_count = 1  # Increment each time we create a new meter
 
-    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
-    def switch_features_handler(self, ev):
-        datapath = ev.msg.datapath
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None, timeout=0):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst, hard_timeout=timeout)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst, hard_timeout=timeout)
+        datapath.send_msg(mod)
 
-        self.add_flow(datapath, 0, match, actions)
+    def add_meter_flow(self, datapath, in_port, src, dst, timeout=10):
+        dpid = datapath.id
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        out_port = self.mac_to_port[dpid][dst]
+        meter_key = (datapath.id, in_port, src, dst)
 
-        # add rule for metering in s1
-        if ev.msg.datapath.id==1:
-            datapath = ev.msg.datapath
-            ofproto = datapath.ofproto
-            parser = datapath.ofproto_parser
-
+        if meter_key not in self.meters:
             bands = [
                 parser.OFPMeterBandDrop(
                     type_=ofproto.OFPMBT_DROP,
@@ -42,42 +54,49 @@ class SimpleSwitch13(app_manager.RyuApp):
                 datapath=datapath,
                 command=ofproto.OFPMC_ADD,
                 flags=ofproto.OFPMF_KBPS,
-                meter_id=1,
+                meter_id=self.meter_count,
                 bands=bands
             )
 
+            # Add a meter the switch
             datapath.send_msg(req)
 
-            #when the traffic generated from h1 will be applied to meter_id=1
-            match = parser.OFPMatch(in_port=1, eth_type=0x0800)
-            #match = parser.OFPMatch(in_port=1, eth_type=0x0800, ipv4_src="10.0.0.1", ipv4_dst="10.0.0.2")
-            actions = [parser.OFPActionOutput(2)]
-            inst = [
-                parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions),
-                parser.OFPInstructionMeter(1,ofproto.OFPIT_METER)
-            ]
-            mod = datapath.ofproto_parser.OFPFlowMod(
-                datapath=datapath, match=match, cookie=0,
-                command=ofproto.OFPFC_ADD, idle_timeout=0,
-                hard_timeout=0, priority=3, instructions=inst
-            )
+        self.meters.add(meter_key)
 
-            datapath.send_msg(mod)
+        match = parser.OFPMatch(in_port=in_port, eth_src=src, eth_dst=dst)
+        actions = [parser.OFPActionOutput(out_port)]
+        inst = [
+            parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions),
+            parser.OFPInstructionMeter(1,ofproto.OFPIT_METER)
+        ]
+        mod = datapath.ofproto_parser.OFPFlowMod(
+            datapath=datapath, match=match, cookie=0,
+            command=ofproto.OFPFC_ADD, idle_timeout=timeout,
+            hard_timeout=0, priority=3, instructions=inst
+        )
 
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-        if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
         datapath.send_msg(mod)
+        self.throttle_info[meter_key]['throttle_started'] = True
+        self.throttle_info[meter_key]['meter_id'] = self.meter_count
+        self.meter_count += 1
+        self.logger.info("Throttle started between %r and %r on dpid %r", src, dst, dpid)
+
+    def should_throttle(self, datapath, in_port, src, dst, timeout=30):
+        current_time = time.time()
+        key = (datapath.id, in_port, src, dst)
+        if key not in self.throttle_info:
+            self.throttle_info[key] = {
+                "detected_time": current_time,
+                "meter_id": None,
+                "throttle_started": False
+            }
+            return False
+        data = self.throttle_info[key]
+        if data['throttle_started']:
+            self.logger.info("Forget throttling has happened between %r and %r on dpid %r", src, dst, datapath.id)
+            self.throttle_info.pop(key)
+            return False
+        return current_time - data['detected_time'] >= timeout
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
@@ -113,14 +132,17 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
-            # verify if we have a valid buffer_id, if yes avoid to send both
-            # flow_mod & packet_out
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
+            if self.should_throttle(datapath, in_port, src, dst):
+                self.add_meter_flow(datapath, in_port, src, dst)
             else:
-                self.add_flow(datapath, 1, match, actions)
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+                # verify if we have a valid buffer_id, if yes avoid to send both
+                # flow_mod & packet_out
+                if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                    self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                    return
+                else:
+                    self.add_flow(datapath, 1, match, actions, timeout=10)
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
